b8d612c5e2170010658674b0475dd7705613078c
Nkenganyi <ntungnkenganyi54@gmail.com>
2022-02-05 10:41:18 +0200

    added the android package

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.nkentech.recipe',
   appName: 'ionic-angular-course-2',
   webDir: 'www',
   bundledWebRuntime: false
